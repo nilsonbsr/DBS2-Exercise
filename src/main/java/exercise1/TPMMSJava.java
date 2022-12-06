3c98a9fe6ca953495c2d19383f5e93400d1ffa02
Peter Braun <peter.braun@student.hpi.de>
2022-12-06 18:56:15 +0100

    commit to pull changes

--- src/main/java/exercise1/TPMMSJava.java
+++ src/main/java/exercise1/TPMMSJava.java
@@ -2,14 +2,12 @@ package exercise1;
 
 import de.hpi.dbs2.ChosenImplementation;
 import de.hpi.dbs2.dbms.*;
+import de.hpi.dbs2.dbms.utils.BlockSorter;
 import de.hpi.dbs2.exercise1.SortOperation;
-import kotlin.Triple;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Array;
-import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Objects;
 
 @ChosenImplementation(true)
 public class TPMMSJava extends SortOperation {
@@ -19,53 +17,69 @@ public class TPMMSJava extends SortOperation {
 
     @Override
     public int estimatedIOCost(@NotNull Relation relation) {
-        throw new UnsupportedOperationException("TODO");
+        int iOCostPerSublist =  3 * relation.getEstimatedSize();
+        return iOCostPerSublist;
     }
 
     @Override
     public void sort(@NotNull Relation relation, @NotNull BlockOutput output) {
 
-        Iterator iter = relation.iterator();
-        Block block = getBlockManager().load(relation.iterator().next());
-        int columnCount = (block.get(0).getColumnCount());
-        getBlockManager().release(block, false);
+       BlockManager blockmanager = getBlockManager();
+       
+        // check if memory is large enough if not throw exception
+        if(relation.getEstimatedSize() > blockmanager.getFreeBlocks() * 3){
+           throw new RelationSizeExceedsCapacityException();
+       }
 
-        for (int i = 0; i < columnCount; i++) {
-            for (Block a : relation) {
-                Block b = getBlockManager().load(a);
+        //Block segmentation
+        int blockSegmentaion = (int)Math.floor(relation.getEstimatedSize() / blockmanager.getFreeBlocks());
 
-                if (b.get(0).get(i) instanceof Integer) {
-                    //System.out.println(b.get(0).get(i).getClass());
+        ArrayList<Block> sublist = new ArrayList<>(blockSegmentaion);
+        ArrayList<Block> block = new ArrayList<>();
+        ColumnDefinition columnDefinition = relation.getColumns();
+        int sortedIndex = getSortColumnIndex();
 
-                } else if (b.get(0).get(i) instanceof Double) {
 
-                } else {
-                }
-                getBlockManager().release(b, false);
+        // in case the blocks ain't empty sort them and wipe them from memory
+        if (!block.isEmpty()){
+            BlockSorter.INSTANCE.sort(relation, block, columnDefinition.getColumnComparator(sortedIndex));
+            for(Iterator<Block> blockIterator = relation.iterator(); blockIterator.hasNext();){
+                Block b = blockIterator.next();
+                blockmanager.release(b, false);
             }
+           // Iterator<Block> blockIterator = relation.iterator();
+            //blockIterator.hasNext();
 
-        /*int i = 0;
-        for (Block a : relation) {
-            Block b = getBlockManager().load(a);
-            System.out.println("Block: " + i +", Tupel 0: " + b.get(0));
-            System.out.println("Block: " + i +", Tupel 1: " + b.get(1));
-            System.out.println("Block: " + i +", Tupel: 0, Column 0: " + b.get(0).get(0));
-            System.out.println("Block: " + i +", Tupel: 0, Column 1: " + b.get(0).get(1));
-            System.out.println("Block: " + i +", Tupel: 0, Column 2: " + b.get(0).get(2));
-            System.out.println();
-            /*Tuple<Int, String, Float> niceTriple = Tuple.with=(363, "ce786853-89b1-3e64-b99a-c98ec095b421", -0.6351907754802413);
-            if (b.get(0) = niceTriple){
-
+        }
+        // for each block in relation we add the blocks in our relation and then if we have free blocks we can sort them by index
+        /*if(blockmanager.getFreeBlocks() != 0){
+            for(Iterator<Block> blockIterator = relation.iterator(); blockIterator.hasNext();){
+                Block b = blockIterator.next();
             }*/
 
+        // iterate through blocks, load and add to current list till full and then move it to disc
+        for(Block blk: relation){
+            Block b = blockmanager.load(blk);
+            block.add(b);
+            //int currentBlockListIndex = 0;
+           // ArrayList<Block> blockElement = new ArrayList<>();
 
-            /*if (b.get(1).get(0) instanceof Integer){
-                System.out.println(b.get(0).get(0).getClass());
+            if(blockmanager.getFreeBlocks() != 0){
+                //blockmanager.load(b);
+                //block.add(b);
+                BlockSorter.INSTANCE.sort(relation, block, columnDefinition.getColumnComparator(sortedIndex));
+                getBlockManager().release(blk, true);
+
             }
-
-            i++;
-            getBlockManager().release(b, false);*/
         }
-        //System.out.println("i count: " + i);
+
+
+
+        }
+
+
+
+
+
     }
-}
+
